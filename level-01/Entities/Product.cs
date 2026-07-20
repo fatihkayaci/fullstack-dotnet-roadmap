009b19de0c7d740f0527e3c7770186a76554fb86
Fatih Kayacı <fatihkayaci@yahoo.com>
2026-07-21 01:52:54 +0300

    git commit -m "level-01: add in-memory CRUD API for Product"

---
+++ level-01/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace level_01.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public double Price { get; set; }
+    public int Stock { get; set; }
+}
